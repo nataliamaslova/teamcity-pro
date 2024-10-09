189a4eb58d1a722872c859b5ce93d44329ddf26e
nataliamaslova <maslova.nd@gmail.com>
2024-10-09 13:16:39 +0200

    Add base for requests, CRUD interface

--- src/main/java/org/example/teamcitypro/api/enums/Endpoint.java
+++ src/main/java/org/example/teamcitypro/api/enums/Endpoint.java
@@ -8,7 +8,7 @@ import org.example.teamcitypro.api.models.BuildType;
 @AllArgsConstructor
 @Getter
 public enum Endpoint {
-    BUILD_TYPES("/app/rest/buildTypes", BuildType.class)
+    BUILD_TYPES("/app/rest/buildTypes", BuildType.class);
     private final String url;
     private final Class <? extends BaseModel> modelClass;
 }
