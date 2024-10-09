2e6d3dbffc82281a02cbd9d3cf26ad4f1cbb8aaf
nataliamaslova <maslova.nd@gmail.com>
2024-10-09 10:37:40 +0200

    Add config, spec, base tests

---
+++ src/main/java/org/example/teamcitypro/api/models/User.java
@@ -0,0 +1,15 @@
+package org.example.teamcitypro.api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String userName;
+    private String password;
+}
