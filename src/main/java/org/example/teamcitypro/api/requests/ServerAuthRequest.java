e175aa3f135c0637ed8781c3dfd9020a54eade28
nataliamaslova <maslova.nd@gmail.com>
2024-11-16 15:50:41 +0100

    Automate setting perProjectPermissions for roles testing Fix access to user entity

---
+++ src/main/java/org/example/teamcitypro/api/requests/ServerAuthRequest.java
@@ -0,0 +1,31 @@
+package org.example.teamcitypro.api.requests;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+import org.example.teamcitypro.api.models.ServerAuthSettings;
+
+public class ServerAuthRequest {
+    public static final String SERVER_AUTH_SETTINGS_URL = "/app/rest/server/authSettings";
+    private RequestSpecification spec;
+    public ServerAuthRequest(RequestSpecification spec) {
+        this.spec = spec;
+    }
+
+    public ServerAuthSettings read() {
+        return RestAssured.given()
+                .spec(spec)
+                .get(SERVER_AUTH_SETTINGS_URL)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(ServerAuthSettings.class);
+    }
+
+    public ServerAuthSettings update(ServerAuthSettings authSettings) {
+        return RestAssured.given()
+                .spec(spec)
+                .body(authSettings)
+                .put(SERVER_AUTH_SETTINGS_URL)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(ServerAuthSettings.class);
+    }
+}
--- src/test/java/org/example/teamcitypro/api/BaseApiTest.java
+++ src/test/java/org/example/teamcitypro/api/BaseApiTest.java
@@ -1,6 +1,40 @@
 package org.example.teamcitypro.api;
 
 import org.example.teamcitypro.BaseTest;
+import org.example.teamcitypro.api.models.AuthModules;
+import org.example.teamcitypro.api.models.ServerAuthSettings;
+import org.example.teamcitypro.api.requests.ServerAuthRequest;
+import org.example.teamcitypro.api.spec.Specifications;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import static org.example.teamcitypro.api.generators.TestDataGenerator.generate;
 
 public class BaseApiTest extends BaseTest {
+    private final ServerAuthRequest serverAuthRequest = new ServerAuthRequest(Specifications.superUserSpec());
+    private AuthModules authModules;
+    private boolean perProjectPermission;
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUpServerAuthSettings() {
+        // Get current settings perProjectPermission
+        perProjectPermission = serverAuthRequest.read().getPerProjectPermissions();
+
+        authModules = generate(AuthModules.class);
+
+        // Update value perProjectPermissions with true
+        serverAuthRequest.update(ServerAuthSettings.builder()
+                        .perProjectPermissions(true)
+                        .modules((authModules))
+                .build());
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void cleanUpServerAuthSettings() {
+        // Update perProjectPermission with initial value
+        serverAuthRequest.update(ServerAuthSettings.builder()
+                .perProjectPermissions(perProjectPermission)
+                .modules((authModules))
+                .build());
+    }
 }
